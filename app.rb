affce3990cf54887cdc7d5bf3931b70f07812497
Erin Stalzer <erinstalzer@gmail.com>
2019-06-06 18:17:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,4 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get "/create_puppy" do
+    erb :create_puppy
+  end
+
+  post "/create_puppy" do
+    
+  end
+
+  get "/display_puppy" do
+    erb :display_puppy
+  end
 end
